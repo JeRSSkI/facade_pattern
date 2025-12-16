e80568ce13df9d3cd383f987d859ab8cf6f5f985
JeRSSkI <13553619@gsuite.duit.edu.ua>
2025-12-16 10:11:20 +0000

    Create sound_system.py

---
+++ sound_system.py
@@ -0,0 +1,9 @@
+class SoundSystem:
+    def on(self):
+        print("Sound System is ON")
+
+    def set_volume(self, level):
+        print(f"Sound volume set to {level}")
+
+    def off(self):
+        print("Sound System is OFF")

da6d5266202d7f9beaa7cf32eb3c746ed0ed1d47
JeRSSkI <13553619@gsuite.duit.edu.ua>
2025-12-16 10:10:49 +0000

    Create dvd_player.py

---
+++ dvd_player.py
@@ -0,0 +1,9 @@
+class DVDPlayer:
+    def on(self):
+        print("DVD Player is ON")
+
+    def play(self, movie):
+        print(f"Playing movie: {movie}")
+
+    def off(self):
+        print("DVD Player is OFF")
